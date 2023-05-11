18f80f48ebd2ea63d05bbc71298c1e79fa2ad8b5
silvio-aguilar <silviocreate@gmail.com>
2023-05-11 15:55:38 +0200

    BaseTerminada

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo from './components/Todo'
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* este es nuestro punto de entrada para la app llamaremos al componente Todo para mostrar el formulario */}
+      <Todo />
     </div>
   );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import { MdOutlineEdit, MdDelete } from "react-icons/md";
+
+import {TiStarOutline, TiStarFullOutline} from 'react-icons/ti'
+
+function TodoList({ todos, updateTodo, removeTodo, completeTodo }) {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: "",
+  });
+
+  const submitUpdate = (value) => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: "",
+    });
+  };
+
+  const [favorito, setFavorito] = useState(false)
+
+  const handleToggleFavorito = (id) => {
+    setFavorito(!favorito)
+  }
+
+if (edit.id) {
+ return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+}
+
+  return todos.map((todo, index) => (
+    <>
+
+<div className="nueva">
+<label>
+        <input className="textoNuevo" type="checkbox" checked={favorito.id} onChange={handleToggleFavorito} />
+        {favorito ? <TiStarFullOutline size={24} /> : <TiStarOutline size={24} />}
+      </label>
+        </div>
+      <div
+        className={todo.isComplete ? "todo-complete" : "todo-container"}
+        key={index}
+      >
+        
+        <div>
+          
+          <input
+            className="cuadradoCheck"
+            type="checkbox"
+            checked={todo.isComplete}
+            onChange={() => completeTodo(todo.id)}
+          />
+          <span
+            onClick={() => completeTodo(todo.id)}
+            style={{ textDecoration: todo.isComplete ? "line-through" : "none" }}
+          >
+
+            
+            {todo.text}
+          </span>
+        </div>
+        
+
+        <div className="icons">
+          <MdDelete
+            onClick={() => removeTodo(todo.id)}
+            className="delete-icon"
+          />
+          <MdOutlineEdit
+            onClick={() => setEdit({ id: todo.id, value: todo.text })}
+            className="edit-icon"
+          />
+        </div>
+      </div>
+    </>
+  ));
+}
+
+export default TodoList;
